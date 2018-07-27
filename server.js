68e04c74366f7bbfe530a8f3552c83361dc97568
Robin Pahlman <robin.pahlman@gmail.com>
2018-07-27 17:46:33 -0400

    set up nodemon

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello'));
+app.get('/', (req, res) => res.send('Hello World!'));
 
+// PORT for Heroku configuration, else 5000
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
